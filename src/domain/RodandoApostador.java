03da8b0301b565b701bf0df19882f60c743ef25b
ericmendescaminha <147173566+ericmendescaminha@users.noreply.github.com>
2024-10-11 21:59:12 -0300

    Ultima versão feita, atualizada com os gitignore e comentarios novos e edição de nome de variavel

--- src/domain/RodandoApostador.java
+++ src/domain/RodandoApostador.java
@@ -1,13 +1,15 @@
 package domain;
 
 import java.util.Random;
+
 // codigo feito para provar a lei dos grandes numeros, o que prova que a casa de apostas nunca sai perdendo se tiver tempo o suficiente
 // nesse caso, para meios meramente ilustrativos, o apostador sempre vai apostar tudo o que ele tem, podendo perder tudo ou duplicar o dinheiro
+// posso alterar o valor da aposta depois
 
 public class RodandoApostador implements Runnable {
 
     private String nome; // Atributo para armazenar o nome do apostador
-    private double apostador = 300; // Saldo inicial do apostador
+    private double saldoApostador = 300; // Saldo inicial do apostador
     private Random random = new Random(); // Instância do gerador de números aleatórios
     private static double saldoBanca = 0;  // Atributo estático compartilhado por todas as threads para o saldo da banca
 
@@ -22,35 +24,41 @@ public class RodandoApostador implements Runnable {
     @Override
     public void run() {
 
-        while (apostador>=1){
+        while (saldoApostador >= 1) {
 
-            int numeroAleatorio = random.nextInt(100) + 1;
+            int numeroAleatorio = random.nextInt(100) + 1; // todo loop novo é gerado um novo numero
 
             if (numeroAleatorio > 49){ //quando ele perde
-                System.out.print(nome + " perdeu uma, saldo era " + apostador + " agora é ");
+                System.out.print(nome + " perdeu uma, saldo era " + saldoApostador + " agora é ");
 
-                atualizarBanca(+apostador); // Atualiza o saldo da banca quando o apostador perde
-                apostador= apostador - apostador;
+                atualizarBanca(+saldoApostador); // Atualiza o saldo da banca quando o apostador perde
 
-                System.out.println(apostador);
+                //saldoApostador =- saldoApostador;
+                saldoApostador -= saldoApostador;
+                //saldoApostador = saldoApostador- saldoApostador;
+
+                System.out.println(saldoApostador);
 
                 System.out.println("Saldo da banca atualizado: " + saldoBanca);
             }
 
             if (numeroAleatorio <= 49){ // quando ele ganha
 
-                System.out.print(nome + " ganhou uma, saldo era " + apostador + " agora é ");
+                System.out.print(nome + " ganhou uma, saldo era " + saldoApostador + " agora é ");
 
-                atualizarBanca(-apostador); // Atualiza o saldo da banca quando o apostador ganha
-                apostador= apostador + apostador;
+                atualizarBanca(-saldoApostador); // Atualiza o saldo da banca quando o apostador ganha
 
-                System.out.println(apostador);
+                //saldoApostador = saldoApostador + saldoApostador;
+                saldoApostador += saldoApostador;
+                //saldoApostador =+ saldoApostador;
+
+                System.out.println(saldoApostador);
 
                 System.out.println("Saldo da banca atualizado: " + saldoBanca);
             }
         }
 
-        if (apostador <= 0 ){
+        if (saldoApostador <= 0) {
             System.out.println(nome + " perdeu tudo");
         }
     }}
--- src/test/main.java
+++ src/test/main.java
@@ -3,16 +3,16 @@ package test;
 import domain.RodandoApostador;
 
 public class main {
-    public static void main(String[] args) { //
+    public static void main(String[] args) {
 
+        for (int i = 1; i <= 100; i++) {
 
-        for (int i = 1; i <= 10; i++) { //
+            String nomeApostador = "Apostador " + i;                                     // Nome do apostador baseado na iteração
+            RodandoApostador rodandoApostador = new RodandoApostador(nomeApostador);     // instanciando a Thread
+            Thread thread = new Thread(rodandoApostador);                                // Passando o Runnable para uma Thread
+            thread.start();                                                              // Iniciando a thread
 
-            String nomeApostador = "Apostador " + i; // Nome do apostador baseado na iteração
-            RodandoApostador rodandoApostador = new RodandoApostador(nomeApostador);
-            Thread thread = new Thread(rodandoApostador); // Passando o Runnable para uma Thread
-            thread.start(); // Iniciando a thread
-
+            // preciso estudar mais thread
         }
     }
 }
